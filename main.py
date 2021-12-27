087e8c1609de325ced6913561824e4e89263d6a2
Kenneth-Okhueleigbe <kenneth_ehimare@yahoo.com>
2021-12-27 15:54:40 +0100

    Changed secret key for flask form

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
-import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
